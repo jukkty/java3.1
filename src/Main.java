34e51d1a083ac429fffa901c990e7b05465f9f05
jukkty <dima.iachimov@gmail.com>
2021-03-10 16:09:47 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 3500;
+        int MilesPerRubles = 20;
+        int totalMiles = ticketPrice / MilesPerRubles;
+        System.out.println(totalMiles);
+    }
+}
